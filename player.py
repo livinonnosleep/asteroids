175fb77e0d033d1c0c44d5b858d7ba8505a73125
tnelson_mcgh <tnelson@mastercontrol.com>
2025-12-19 14:03:49 -0700

    D: correct player.py

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
-from constants import *
-from circleshape import *
+from constants import PLAYER_RADIUS, LINE_WIDTH
+from circleshape import CircleShape
 class Player(CircleShape):
     def __init__ (self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
