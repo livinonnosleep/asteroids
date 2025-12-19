1f948ed64c42c8e77687cda031334f8a1dabe3a1
tnelson_mcgh <tnelson@mastercontrol.com>
2025-12-19 14:00:52 -0700

    C: new branch commit

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
 
